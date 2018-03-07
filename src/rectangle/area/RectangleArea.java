b751ee65b16107700f43cea11c8fa71f3aecad30
seden0957 <seden0957@7KXL6Y1.Student.UGDSB.ED>
2018-03-07 13:59:37 -0500

    7head

---
+++ src/rectangle/area/RectangleArea.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rectangle.area;
+
+
+/**
+ *
+ * @author seden0957
+ */
+public class RectangleArea {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double width, length, area;
+        width = 5.7;
+        length = 4.8;
+        area = length * width;
+        System.out.println(area);
+    }
+    
+}
